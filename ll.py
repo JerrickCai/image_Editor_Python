8d6441aa75d048e10b191bd44d44aa27e42d52b2
Jiali Cai <jialic@sfu.ca>
2019-12-17 21:00:42 -0800

    v0.2 now pictures are set in the middle of the canvas, and the picture will be resized according to the ratio of picture

--- ll.py
+++ ll.py
@@ -9,51 +9,64 @@ class myGUI(object):
         # create a window
         self.root = Tk()
         self.root.title('image editor')
-        self.root.geometry('800x900')
-        self.root.configure(background='grey')
-        self.canvas = Canvas(width=800, height=800, bg='white')
-        self.canvas.pack()
+        self.root.resizable(width=False, height=False)
 
-        filename='1.jpg'
-        image = Image.open(filename)
-        self.photo = ImageTk.PhotoImage(image)
-        self.img = self.canvas.create_image(250, 250, image=self.photo)
+        canvasW = 800
+        canvasH = 800
+        # self.root.configure(background='grey')
+        self.canvas = Canvas(width=canvasW, height=canvasH, bg='gray')
+        self.canvas.pack(fill='both', expand='yes')
+
+        path = 'default.jpg'
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        print(img.shape)
+        img = self.checkSize(img)
+        print(img.shape)
+        self.photo = ImageTk.PhotoImage(image=Image.fromarray(img))
+        self.img = self.canvas.create_image(canvasH/2, canvasW/2, anchor='center', image=self.photo)
 
         self.text = StringVar()
         # create frame to put control buttons onto
-        self.frame = Frame(self.root, bg='grey', width=800, height=100)
-        self.frame.pack(fill='x')
+        self.frameBottom = Frame(self.root, width=800, height=50)
+        self.frameBottom.pack(fill='both', expand='yes')
+
+        # create frame to put status bar onto
+        self.frameStatus = Frame(self.root, width=800, height=50)
+        self.frameStatus.pack(fill='both', expand='yes')
 
         # create label, which shows status
-        self.statusLabel = Label(self.frame, textvariable=self.text)
-        self.statusLabel.pack(side='left', padx=10)
+        self.statusLabel = Label(self.frameStatus, textvariable=self.text, bd = 1,relief = SUNKEN, anchor = W)
+        self.statusLabel.pack(side=BOTTOM, fill = X)
 
         # create import image button
-        self.importImageButton = Button(self.frame , text="import image", command=self.importImg)
-        self.importImageButton.pack(side='left')
+        self.importImageButton = Button(self.frameBottom , text="import image", command=self.importImg)
+        self.importImageButton.pack(side='left', padx=10)
 
         self.root.mainloop()
-        self.root.destroy()
+        # self.root.destroy()
 
     def importImg(self):
         path = filedialog.askopenfilename()
         img = cv2.imread(path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = self.checkSize(img)
         self.photo = ImageTk.PhotoImage(image=Image.fromarray(img))
         self.canvas.itemconfig(self.img, image=self.photo)
         self.text.set('imported')
 
+    def checkSize(self,img):
+        h, w = img.shape[:2]
+        ratio1 = h/w
+        ratio2 = w/h
+        self.root.update()
+        canvasWidth = self.canvas.winfo_width()
+        canvasHeight = self.canvas.winfo_height()
+        if h>w:
+            img = cv2.resize(img, (int(canvasHeight*ratio2), canvasHeight))
+        else:
+            img = cv2.resize(img, (canvasWidth, int(canvasWidth*ratio1)))
+        return img
+
 if __name__ == "__main__":
     gui = myGUI()
-
-# img = cv2.imread('Scan 1.jpeg')
-# # cv2.imshow('src',img)
-# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# img4 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-# cv2.imshow('src',gray)
-# print(img.shape)
-# print(img.size) # 像素总数目
-# print(img.dtype)
-# print(img)
-#
-# cv2.waitKey()
